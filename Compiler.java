cc1cd961881a8afc71fbeade5aac2f944080e346
trorik23 <trorik23@gmail.com>
2016-11-13 02:22:54 -0300

    Super mega commit
    Corto aca porque me muero de sueño.Por ahora no anda nada.
    Se esta modularizando todo el codigo, no se busca la eficiencia por ahora.
    Se agregó un reporte de errores de compilacion.
    El codigo queda a medias y con muchos errores, pero solo porque se esta refactorizando.

--- Compiler.java
+++ Compiler.java
@@ -8,69 +8,112 @@ public class Compiler {
 	 * 1 instruccion por linea
 	 * Debe haber espacios entre las palabras clave
 	 * Se acepta la ausencia de un espacio luego del ":" de etiquetas y variables
-	 * Si no se define el valor de una variable se la considera =0
+	 * 
+	 * Comentarios #<comentarios>
 	 *  
 	 * 
 	 * 
 	 */
-	public static void main(String[] args) {
-		
+
+	//TODO chequear uniqueness
+
+
+	//Definicion tabla de codOps
+	//La misma contendrá tambien las referecias de etiquetas
+	static HashMap<String, String> codeOps = HashMapBuilder.build("LOAD", "00000010", "ADD", "00000011", "STORE", "00000001", "JUMP", "00000000",
+			"SUB", "00000110", "AND", "00000100", "JZ", "00000101", "NOP", "00000111", "HALT", "00001000");
+
+	//Memoria de 256 celdas
+	static String[] programa = new String[256];
+
+	//En labels se almacenarán las variables
+	static ArrayList<String> variables = new ArrayList<String>();
+
+	//Codigo pasado en limpio
+	static ArrayList<String> codigoFuente= new ArrayList<String>();
+
+	//Mensajes con errores de compilacion
+	static ArrayList<String> erroresCompilacion =new ArrayList<String>();
+
+	private static void chequeoDeclaracionCuerpoVariablesPrograma(){
+
+		if(!codigoFuente.contains("var:")){
+			erroresCompilacion.add("Falta la declaracion 'var:'");
+		}else{
+			if(codigoFuente.indexOf("var:")!=0){
+				erroresCompilacion.add("El codigo debe empezar con 'var:'");
+			}
+			if(!codigoFuente.contains("endvar")){
+				erroresCompilacion.add("Falta la declaracion 'endvar'");
+			}else{
+				//tiene ambas
+				if(codigoFuente.indexOf("var:")>codigoFuente.indexOf("endvar")){
+					erroresCompilacion.add("'endvar' debe declararse luego de 'var:'");
+				}	
+			}
+		}
+
+		if(!codigoFuente.contains("programa:")){
+			erroresCompilacion.add("Falta la declaracion 'programa:'");
+		}else{
+			if(!codigoFuente.contains("end")){
+				erroresCompilacion.add("Falta la declaracion 'end'");
+			}else{
+				//tiene ambas
+				if(codigoFuente.indexOf("programa:")>codigoFuente.indexOf("end")){
+					erroresCompilacion.add("'end' debe declararse luego de 'programa:'");
+				}	
+			}
+		}
+
+		if(codigoFuente.indexOf("var:")>codigoFuente.indexOf("programa:")){
+			erroresCompilacion.add("Se deben declarar las variables antes del programa principal");
+		}
+	}
+
+	public static void main(String[] args) {		
 		//Lectura del nombre del archivo
 		Scanner inp = new Scanner(System.in);
 		System.out.print("Ingrese el nombre del archivo a compilar: ");
 		String fileName = inp.next();
-		
-		//Configuracion de lecutura y escritura de archivos 
+
+		//Configuracion de lecutura y escritura de archivos
+		//TODO manejar bien las extensiones
 		Scanner scn = IOFileManager.IO(fileName, fileName + ".taq");
 		if(scn == null) {
 			System.err.println("Error en la carga del archivo");
 			inp.close();
 			return;
 		}
-		
-		//Definicion tabla de codOps
-		//La misma contendrá tambien las referecias de etiquetas
-		HashMap<String, String> codeOps = HashMapBuilder.build("LOAD", "00000010", "ADD", "00000011", "STORE", "00000001", "JUMP", "00000000",
-				"SUB", "00000110", "AND", "00000100", "JZ", "00000101", "NOP", "00000111", "HALT", "00001000");
-		
-		//Inicializacion de la tabla de memoria de 256 celdas
-		String[] program = new String[256];
-		for (int i = 0; i < program.length; i++) {
-			program[i] = "0000000000000000";
-		}
-		
-		//En labels se almacenarán las variables
-		ArrayList<String> labels = new ArrayList<String>();
-		
 
-		// Variables loading
-		boolean stopError=false;
-		boolean stop = false;
-		boolean varRead=false;
-		while(scn.hasNext() && !stop && !stopError){
-			String instruction = scn.nextLine();
-			if(instruction.equalsIgnoreCase("var:") && !varRead){
-				varRead=true;
-			}else if(!varRead) {
-				stopError=true;
-			} else if(instruction.equalsIgnoreCase("endvar")){
-				stop=true;
-			} else if(!instruction.trim().equalsIgnoreCase("")){
-				instruction = instruction.replaceAll(" ", ""); // cleanup so that it's like this: 'label:value'
-				String label = instruction.split(":")[0];
-				// TODO: validate_uniqueness_of :label
-				int value = Integer.parseInt(instruction.split(":")[1]);
-				labels.add(label);
-				program[256 - labels.size()] = String.format("%16s", Integer.toString(value, 2)).replace(" ", "0");
+		//Inicializacion de la tabla de memoria de 256 celdas		
+		for (int i = 0; i < programa.length; i++) {
+			programa[i] = "0000000000000000";
+		}
+
+
+		//Compilacion
+		cargaCodigoFuente(scn);
+		chequeoDeclaracionCuerpoVariablesPrograma();
+		if(erroresCompilacion.isEmpty()){
+			cargaVariables();
+			cargaPrograma();
+		}	
+
+		//Reporte
+		if(erroresCompilacion.isEmpty()){
+			System.err.println("Compilacion exitosa");
+		}else{
+			System.err.println(erroresCompilacion.size()+" errores de compilacion.\n");
+			for(String s:erroresCompilacion){
+				System.err.println(s);
 			}
 		}
-		if(stopError) {
-			// SE PRENDIO FUEGO EL PROCESADOR Y DESAPROBASTE TALLER
-			System.err.println("Error de compilacion");
-			return;
-		}
-		
-		
+
+
+
+
+
 		// Program compilation
 		boolean end = false;
 		boolean programRead = false;
@@ -88,10 +131,10 @@ public class Compiler {
 				if(decodedInst.length > 2){
 					// Es una inst con label
 					codeOps.put(decodedInst[0].replaceAll(":", ""), String.format("%8s", Integer.toString(lastIndex, 2)).replaceAll(" ", "0"));
-					
+
 				} else if(decodedInst.length == 1){
 					// Es halt
-					program[lastIndex++] = codeOps.get(decodedInst[0]) + "00000000";
+					programa[lastIndex++] = codeOps.get(decodedInst[0]) + "00000000";
 					continue;
 				}
 				String codeOp = decodedInst[decodedInst.length-2];
@@ -101,7 +144,7 @@ public class Compiler {
 					lastIndex++;
 				}else {
 					op = decodedInst[decodedInst.length-1];
-					program[lastIndex++] = codeOps.get(codeOp) + String.format("%8s", Integer.toString(255 - labels.indexOf(op), 2)).replaceAll(" ", "0");
+					programa[lastIndex++] = codeOps.get(codeOp) + String.format("%8s", Integer.toString(255 - variables.indexOf(op), 2)).replaceAll(" ", "0");
 				}
 			}
 		}
@@ -110,7 +153,7 @@ public class Compiler {
 		scn=IOFileManager.IO(fileName, fileName+".taq");
 		while(!scn.nextLine().equalsIgnoreCase("programa:") && !end && !stopError){
 		}
-		
+
 		while(scn.hasNext() && !end && !stopError){
 			String instruction = scn.nextLine();
 			if(instruction.equalsIgnoreCase("end")){
@@ -121,16 +164,16 @@ public class Compiler {
 					String codeOp = decodedInst[decodedInst.length-2];
 					if(codeOp.equalsIgnoreCase("jump") || codeOp.equalsIgnoreCase("jz")){
 						String op = decodedInst[decodedInst.length-1];
-						program[lastIndex] = codeOps.get(codeOp) + codeOps.get(op);
+						programa[lastIndex] = codeOps.get(codeOp) + codeOps.get(op);
 					}
 				}else{
 				}
 				lastIndex++;
 			}
 		}
-		for (int i = 0; i < program.length; i++) {
-			if(program[i].equalsIgnoreCase(codeOps.get("JUMP")) || program[i].equalsIgnoreCase(codeOps.get("JZ"))){
-				program[i] = program[i].substring(0, -2) + codeOps.get(program[i].substring(-2, -1));
+		for (int i = 0; i < programa.length; i++) {
+			if(programa[i].equalsIgnoreCase(codeOps.get("JUMP")) || programa[i].equalsIgnoreCase(codeOps.get("JZ"))){
+				programa[i] = programa[i].substring(0, -2) + codeOps.get(programa[i].substring(-2, -1));
 			}
 		}
 		if(stopError) {
@@ -138,12 +181,46 @@ public class Compiler {
 			System.err.println("Error de compilacion");
 			return;
 		}
-		for (int i = 0; i < program.length; i++) {
-			System.out.println(program[i]);
+		for (int i = 0; i < programa.length; i++) {
+			System.out.println(programa[i]);
 		}
-		
-		
+
+
 		inp.close();
+	}
+
+	private static void cargaVariables(){
+		//En este punto se supone que codigoFuente[0]="var:" por lo tanto se ignora
+		int i=1;
+		while(!codigoFuente.get(i).equalsIgnoreCase("endvar")){
+			String var=codigoFuente.get(i).replaceAll(" ","").split(":")[0];
+			if(variables.contains(var)){
+				erroresCompilacion.add("No se pueden definiar variables con el mismo nombre");
+			}else{
+				try{
+					Integer num=Integer.parseInt(codigoFuente.get(i).replaceAll(" ","").split(":")[1]);
+					variables.add(var);
+					programa[256-variables.size()]=String.format("%16s", Integer.toString(num, 2)).replace(" ", "0");
+				}catch(NumberFormatException e){
+					erroresCompilacion.add("Las variables deben inicializarse con un valor decimal");
+				}
+			}
+			i++;
+		}		
+	}
+
+	private static void cargaPrograma(){
+
+	}
+
+	private static void cargaCodigoFuente(Scanner scn){
+		//se limpiarán lineas vacias,tabs,espacios de sobra y comentarios
+		while(scn.hasNext()){
+			String instruction = scn.nextLine().trim().replace("\t","").split("#")[0];
+			if(!instruction.equalsIgnoreCase("")){
+				codigoFuente.add(instruction);
+			}			
+		}		
 	}
 
 }
